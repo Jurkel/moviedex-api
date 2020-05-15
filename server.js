61db8251685238834eddd902db149057608d5f96
Harvey Le <harveyxcle@gmail.com>
2020-05-15 13:18:16 -0400

    updated gitignore

--- server.js
+++ server.js
@@ -14,11 +14,9 @@ app
   .use(cors())
   .use(helmet())
 
-app.use(function valideateBearerToken(req, res, next) {
+app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
-  
-  console.log('validate bearer token middleware')
     
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
